53901e3ccee29165f5ef04d763d2dcbbd07cf8ad
Sam Gotowka <sam.gotowka@gmail.com>
2022-08-17 14:30:12 -0400

    commit tree e0d494fd7a38e0cdb9bf0f3672e227ba36ee8c76

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  // method: "POST" is missing from the object below
+  const configurationObject = {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
